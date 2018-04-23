ccb0fd958a80ea268990c443b7561c8fd8ed646a
rushi98 <rushikeshjogdand1@gmail.com>
2018-04-24 00:18:31 +0530

    packaging, deep links
    Refactored `HomeActivity.java` to `home` package.
    Added exmple of deep link.

---
+++ app/src/main/java/com/macbitsgoa/events/EventsApplication.java
@@ -0,0 +1,13 @@
+package com.macbitsgoa.events;
+
+import android.app.Application;
+
+/**
+ * @author Rushikesh Jogdand.
+ */
+public class EventsApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/macbitsgoa/events/home/HomeActivity.java
+++ app/src/main/java/com/macbitsgoa/events/home/HomeActivity.java
@@ -1,26 +1,32 @@
-package com.macbitsgoa.events;
+package com.macbitsgoa.events.home;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import com.macbitsgoa.events.BuildConfig;
+import com.macbitsgoa.events.R;
 
 /**
  * Main Activity of the App.
  *
  * @author Rushikesh Jogdand [rushikeshjogdand1@gmail.com]
  */
+@SuppressLint("GoogleAppIndexingApiWarning")
 public class HomeActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        TextView dummyTv = findViewById(R.id.tv_dummy);
+        final TextView dummyTv = findViewById(R.id.tv_dummy);
 
         // example use of build config variable
         if (BuildConfig.eateries) {
             dummyTv.setText(R.string.test_string_eateries);
         }
+
     }
 }
