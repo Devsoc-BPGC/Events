d560ffb45146b4e2e7f791125c0ad4d1ddced99c
Rushikesh Jogdand <rushikeshjogdand1@gmail.com>
2018-05-14 09:25:25 +0530

    added vertical scrolling view in home activity
    fixes issue #1
    The eateries feature is for demonstration purpose only and will be
    deleted once other features are added. (Of course that feature can be
    eateries itself!)

---
+++ app/src/main/java/com/macbitsgoa/events/eateries/EateriesActivity.java
@@ -0,0 +1,16 @@
+package com.macbitsgoa.events.eateries;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.macbitsgoa.events.R;
+
+public class EateriesActivity extends Activity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eateries);
+        if (getActionBar() != null) getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
