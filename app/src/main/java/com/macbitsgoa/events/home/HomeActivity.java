0a861b7751227e4df31ed2e4ed881392f1bc22d6
Omkar Kanade <omukanade@gmail.com>
2018-05-29 20:16:04 +0530

    share app feature (#27)
    * added nav bar
    
    * share app feature

--- app/src/main/java/com/macbitsgoa/events/home/HomeActivity.java
+++ app/src/main/java/com/macbitsgoa/events/home/HomeActivity.java
@@ -1,14 +1,19 @@
 package com.macbitsgoa.events.home;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
 import com.macbitsgoa.events.BuildConfig;
 import com.macbitsgoa.events.R;
 import com.macbitsgoa.events.aboutfest.AboutFestCardFragment;
 import com.macbitsgoa.events.aboutmac.AboutMacCardFragment;
 import com.macbitsgoa.events.eateries.EateriesCardFragment;
 
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 
@@ -18,9 +23,11 @@ import androidx.fragment.app.FragmentManager;
  * @author Rushikesh Jogdand [rushikeshjogdand1@gmail.com]
  */
 @SuppressLint("GoogleAppIndexingApiWarning")
-public class HomeActivity extends FragmentActivity {
+public class HomeActivity extends FragmentActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
 
     private static boolean areFeaturesPopulated = false;
+
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -29,6 +36,11 @@ public class HomeActivity extends FragmentActivity {
         if (!areFeaturesPopulated) {
             populateFeatures();
         }
+
+        NavigationView navigationView;
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     private void initViews() {
@@ -70,4 +82,46 @@ public class HomeActivity extends FragmentActivity {
         }
         areFeaturesPopulated = true;
     }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+
+            case (R.id.nav_share_app): {
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT, (getString(R.string.test_string_playstore_prompt)
+                        +getString(R.string.mac_playstore_url)));
+
+                shareIntent.setType(getString(R.string.test_string_intent_type));
+
+                startActivity(Intent.createChooser(shareIntent, getString(R.string.test_string_share_app)));
+                break;
+            }
+
+
+            default: {
+                break;
+            }
+        }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
