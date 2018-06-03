53c3c2bf1e77bf03c1ddfc4b1e57a2f2dc1272fd
rrgodhorus <rrgodhorus@gmail.com>
2018-06-03 16:34:51 +0530

    Fix minor issues

--- app/src/main/java/com/macbitsgoa/events/maps/MapsActivity.java
+++ app/src/main/java/com/macbitsgoa/events/maps/MapsActivity.java
@@ -9,7 +9,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.macbitsgoa.events.R;
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,9 +19,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
+
+import com.macbitsgoa.events.FirebaseKeys;
+import com.macbitsgoa.events.R;
+
+import static com.macbitsgoa.events.Utilities.TAG_PREFIX;
 
 /**
  * Google Maps Activity showing Event Markers.
@@ -32,21 +38,13 @@ import androidx.fragment.app.FragmentActivity;
 @SuppressWarnings("DuplicateStringLiteralInspection")
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private static final int MAP_INIT_CAP = 15;  //initial capacity for number of venues
-    private static final String MAP_CENTRAL_LOC = "/locations/central";
-    private static final String MAP_ZOOM_SETTINGS = "/locations/zoom";
-    private static final String MAP_MARKERS_LOC = "/locations/locations";
-    private static final float MAP_DEFAULT_ZOOM = 16.0f;
-    private static final Venue MAP_DEFAULT_CENTRAL = new Venue("15.392199","73.878609","BITS GOA");
-    private static final String FIREBASE_ERROR_TAG = "MapsActivity";
-    private static final String FIREBASE_ERROR_STRING = "Firebase data retrieval failed";
-    private static final String FIREBASE_STRING_LATITUDE = "latitude";
-    private static final String FIREBASE_STRING_LONGITUDE = "longitude";
-    private static final String FIREBASE_STRING_VENUE_NAME = "venueName";
-
-    private float zoomLevel = MAP_DEFAULT_ZOOM;
-    @SuppressWarnings("TypeMayBeWeakened")
-    private final ArrayList<Venue> venues  = new ArrayList<>(MAP_INIT_CAP);
+    private static final int INIT_CAP = 15;  //initial capacity for number of venues
+    private static final float DEFAULT_ZOOM = 16.0f;
+    private static final Venue DEFAULT_CENTRAL = new Venue("15.392199","73.878609","BITS GOA");
+    private static final String FIREBASE_ERROR_TAG = TAG_PREFIX + " "
+                                                    + MapsActivity.class.getSimpleName();
+    private float zoomLevel = DEFAULT_ZOOM;
+    private final Collection<Venue> venues  = new ArrayList<>(INIT_CAP);
     private GoogleMap map;
 
     @Override
@@ -66,8 +64,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -83,7 +80,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void getLocations() {
 
         DatabaseReference reference;
-        reference = FirebaseDatabase.getInstance().getReference(MAP_ZOOM_SETTINGS);
+        reference = FirebaseDatabase.getInstance().getReference(FirebaseKeys.MAP_ZOOM_SETTINGS);
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull final DataSnapshot dataSnapshot) {
@@ -95,20 +92,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             @Override
             public void onCancelled(@NonNull final DatabaseError databaseError) {
-                Log.e(FIREBASE_ERROR_TAG,FIREBASE_ERROR_STRING + " in zoom");
+                Log.e(FIREBASE_ERROR_TAG,databaseError.getMessage(),databaseError.toException());
             }
         });
 
-        reference = FirebaseDatabase.getInstance().getReference(MAP_CENTRAL_LOC);
+        reference = FirebaseDatabase.getInstance().getReference(FirebaseKeys.MAP_CENTRAL_LOC);
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull final DataSnapshot dataSnapshot) {
-                Venue centralVenue = MAP_DEFAULT_CENTRAL;
+                Venue centralVenue = DEFAULT_CENTRAL;
                 for (final DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     centralVenue = new Venue(
-                            snapshot.child(FIREBASE_STRING_LATITUDE).getValue(String.class),
-                            snapshot.child(FIREBASE_STRING_LONGITUDE).getValue(String.class),
-                            snapshot.child(FIREBASE_STRING_VENUE_NAME).getValue(String.class)
+                            snapshot.child(FirebaseKeys.MAP_LATITUDE).getValue(String.class),
+                            snapshot.child(FirebaseKeys.MAP_LONGITUDE).getValue(String.class),
+                            snapshot.child(FirebaseKeys.MAP_VENUE_NAME).getValue(String.class)
                                         );
 
                 }
@@ -120,20 +117,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             @Override
             public void onCancelled(@NonNull final DatabaseError databaseError) {
-                Log.e(FIREBASE_ERROR_TAG,FIREBASE_ERROR_STRING + " in central");
+                Log.e(FIREBASE_ERROR_TAG,databaseError.getMessage(),databaseError.toException());
             }
         });
 
-        reference = FirebaseDatabase.getInstance().getReference(MAP_MARKERS_LOC);
+        reference = FirebaseDatabase.getInstance().getReference(FirebaseKeys.MAP_MARKERS_LOC);
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull final DataSnapshot dataSnapshot) {
                 venues.clear();
                 for (final DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     venues.add(new Venue(
-                            snapshot.child(FIREBASE_STRING_LATITUDE).getValue(String.class),
-                            snapshot.child(FIREBASE_STRING_LONGITUDE).getValue(String.class),
-                            snapshot.child(FIREBASE_STRING_VENUE_NAME).getValue(String.class)
+                            snapshot.child(FirebaseKeys.MAP_LATITUDE).getValue(String.class),
+                            snapshot.child(FirebaseKeys.MAP_LONGITUDE).getValue(String.class),
+                            snapshot.child(FirebaseKeys.MAP_VENUE_NAME).getValue(String.class)
                                     )
                     );
 
@@ -149,7 +146,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             @Override
             public void onCancelled(@NonNull final DatabaseError databaseError) {
-                Log.e(FIREBASE_ERROR_TAG,FIREBASE_ERROR_STRING + " in markers");
+                Log.e(FIREBASE_ERROR_TAG,databaseError.getMessage(),databaseError.toException());
             }
         });
 
